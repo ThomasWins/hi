1a526e18c14279fe223a06f5e6b54b06a2fa34d9
Thomas Winslow <twinslow128@gmail.com>
2024-06-15 09:46:46 -0400

    hi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,16 +11,16 @@ public class Main {
     //
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    //
-    // try {
-    //   serverSocket = new ServerSocket(4221);
-    //   // Since the tester restarts your program quite often, setting SO_REUSEADDR
-    //   // ensures that we don't run into 'Address already in use' errors
-    //   serverSocket.setReuseAddress(true);
-    //   clientSocket = serverSocket.accept(); // Wait for connection from client.
-    //   System.out.println("accepted new connection");
-    // } catch (IOException e) {
-    //   System.out.println("IOException: " + e.getMessage());
-    // }
+   
+    try {
+    serverSocket = new ServerSocket(4221);
+     // Since the tester restarts your program quite often, setting SO_REUSEADDR
+     // ensures that we don't run into 'Address already in use' errors
+    serverSocket.setReuseAddress(true);
+    clientSocket = serverSocket.accept(); // Wait for connection from client.
+    System.out.println("accepted new connection");
+    } catch (IOException e) {
+    System.out.println("IOException: " + e.getMessage());
+    }
   }
 }
